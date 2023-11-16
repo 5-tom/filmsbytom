11bf4f530acac60ccfcb90f20ec4da5566b906e7
tomhonour <tom.honour@illuminetsolutions.com>
2023-11-16 09:26:04 +0000

    Log error for debugging

--- netlify/functions/trains.js
+++ netlify/functions/trains.js
@@ -12,7 +12,8 @@ app.get("/trains", async (req, res) => {
 		const bookingWindowRegex = /{"bookingWindows":.*}/;
 		const bookingWindow = ticketAlert.data.match(bookingWindowRegex);
 		return res.json(JSON.parse(match[0]));
-	} catch {
+	} catch (err) {
+		console.log(err);
 		return res.send("Failed to get data.");
 	}
 });
