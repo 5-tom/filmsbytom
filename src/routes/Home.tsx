f5fe7ecc9e3c917c58b6694e39ed29f4a58e1b04
5-tom <tom5@posteo.net>
2024-03-05 21:02:18 +0000

    Validate form data with zod and zodix

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import { Button, Dialog, TextField } from "@mui/material";
+import { z } from "zod";
+import { parseFormSafe } from "zodix";
 
 export default function Home() {
 	const [open, setOpen] = useState(false);
 	const [request, setRequest] = useState({
 		body: new FormData(),
-		route: ""
+		route: "",
+		schema: z.object({})
 	});
+	const initErrors: {
+		[key: string]: string | null;
+	} = {
+		fname: null
+	};
+	const [errors, setErrors] = useState(initErrors);
 	const [response, setResponse] = useState("");
 
 	function handleClose() {
@@ -14,6 +23,19 @@ export default function Home() {
 	}
 
 	async function submit() {
+		setErrors(initErrors);
+		const result = await parseFormSafe(request.body, request.schema);
+		if (!result.success) {
+			const fResult: { [key: string]: Array<string> } =
+				result.error.flatten().fieldErrors;
+			const zErrors = initErrors;
+			for (const key in fResult) {
+				zErrors[key] = fResult[key][0];
+			}
+			setErrors(zErrors);
+			setOpen(false);
+			return;
+		}
 		fetch(request.route, {
 			method: "post",
 			body: request.body
@@ -31,12 +53,18 @@ export default function Home() {
 					e.preventDefault();
 					setRequest({
 						body: new FormData(e.currentTarget),
-						route: "/api/form"
+						route: "/api/form",
+						schema: z.object({ fname: z.string().email() })
 					});
 					setOpen(true);
 				}}
 			>
-				<TextField name="fname" required />
+				<TextField
+					name="fname"
+					required
+					error={errors.fname ? true : false}
+					helperText={errors.fname ?? errors.fname}
+				/>
 				<Button type="submit">Submit</Button>
 			</form>
 			<span>Response:</span>
