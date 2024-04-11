7949f56e4b4e6819a08d389c355d02efe8ba8e75
5-tom <tom5@posteo.net>
2024-04-11 12:09:31 +0100

    Start of portfolio

---
+++ portfolio/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+	server: {
+		proxy: {
+			"/api": "http://localhost:3000"
+		}
+	},
+	plugins: [react()],
+	build: {
+		outDir: "..",
+		rollupOptions: {
+			input: {
+				app: "portfolio.html"
+			}
+		}
+	}
+});
